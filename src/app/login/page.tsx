e0bced8945b7810a2ec52057aa7241e877f7a873
Thitipat <thitipat@riverpark.co.th>
2024-08-29 02:53:48 +0700

    feat: new login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,27 @@
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center min-h-screen w-full bg-orange-300">
+      <div className="w-full max-w-md bg-white pt-6 px-6 pb-10 sm:px-12 mx-6 sm:mx-0 rounded-xl shadow-lg">
+        <div className="mb-6 text-center">
+          <b className="text-xl">Login</b>
+        </div>
+        <div className="mb-6">
+          <Input
+            type="text"
+            label="Username"
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="mb-6 sm:mb-12">
+          <Input type="password" label="Password" placeholder="********" />
+        </div>
+        <Button className="w-full text-white text-sm" color="warning">
+          Submit
+        </Button>
+      </div>
+    </main>
+  );
+}
